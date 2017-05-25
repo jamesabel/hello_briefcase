69f876bde46a877349ecc711a8d3890e5ac9624a
james abel <j@abel.co>
2017-05-25 12:08:27 -0700

    check python versions too make code a little more clear

--- compiled_package_fixup.py
+++ compiled_package_fixup.py
@@ -3,6 +3,7 @@ import os
 import re
 import shutil
 
+
 def fixup():
     """
     workaround for https://github.com/pybee/briefcase/issues/44
@@ -13,26 +14,34 @@ def fixup():
     if sys.platform == 'darwin':
         # OSX/MacOS
 
-        compiled_ver_and_flag = '%s%s%s' % (sys.version_info.major, sys.version_info.minor, sys.abiflags)
+        compiled_ver = '%s%s' % (sys.version_info.major, sys.version_info.minor)
+        compiled_ver_and_flag = '%s%s' % (compiled_ver, sys.abiflags)
         print('compiled_ver_and_flag : %s' % compiled_ver_and_flag)
 
         # look for all the package compile flags
+        # example of a compiled binary filename:
         # _cffi_backend.cpython-35m-darwin.so
 
-        for root, dirs, files in os.walk(os.path.join('macOS', '')):
+        for root, _, files in os.walk('macOS'):
             for file in files:
                 if 'app_packages' in root:
-                    result = re.match('([\w_\.]*)(cpython-)([\d\w]+)(-darwin.so)', file)
-                    if result:
-                        package_ver_and_flag = result.group(3)
+                    file_match = re.match('([\w_\.]*)(cpython-)(\d+)(\w*)(-darwin.so)', file)
+                    if file_match:
+                        package_ver = file_match.group(3)
+                        if compiled_ver != package_ver:
+                            print('error : compiled and package versions are different (compiled : %s, package : %s)' %
+                                  (compiled_ver, package_ver))
+                        package_flag = file_match.group(4)
+                        package_ver_and_flag = package_ver + package_flag
                         full_path = os.path.join(root, file)
                         print('%s : package_ver_and_flag : %s' % (full_path, package_ver_and_flag))
                         if package_ver_and_flag != compiled_ver_and_flag:
-                            print('flag mismatch between python "%s" and package "%s" - fixing up' %
-                                  (compiled_ver_and_flag, package_ver_and_flag))
-                            fixup_path = os.path.join(root, file.replace(result.group(3), compiled_ver_and_flag))
-                            print('copying %s to %s' % (full_path, fixup_path))
-                            shutil.copy2(full_path, fixup_path)
+                            fixup_path = os.path.join(root, file.replace(package_ver_and_flag, compiled_ver_and_flag))
+                            if not os.path.exists(fixup_path):
+                                print('flag mismatch between python "%s" and package "%s" - fixing up' %
+                                      (compiled_ver_and_flag, package_ver_and_flag))
+                                print('copying %s to %s' % (full_path, fixup_path))
+                                shutil.copy2(full_path, fixup_path)
 
 if __name__ == '__main__':
     fixup()
